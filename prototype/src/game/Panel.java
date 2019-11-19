135d959a7b5a359b047e669090f5ab877a04dece
admin <admin@DESKTOP-62MKJ6O>
2019-11-19 10:52:59 -0600

    hi

--- prototype/src/game/Panel.java
+++ prototype/src/game/Panel.java
@@ -21,19 +21,36 @@ import javax.swing.JPanel;
 
 public class Panel extends JPanel implements KeyListener {
 
+	String screenTracker;
+	
+	
+	String[] screenList = {"title_screen", "select_screen"};
 	
 	public Panel() {
 		addKeyListener(this);
 		setBackground(new Color(255, 255, 255));
 		setDoubleBuffered(true);
 		setFocusable(true);
-		
+		screenTracker = "title_screen";
 		repaint();
 		
 	}
 	
 	public void run() {
 		repaint();
+	}
+	
+	public void drawLoadingScreen(Graphics g) {
+		g.setFont(new Font("Arial", Font.PLAIN, 100));
+		g.setColor(new Color(0, 0, 0));
+		g.drawString("The Closet App", 600, 100);
+		
+		
+		g.setColor(new Color(183, 16, 37));
+		g.setFont(new Font("Arial", Font.PLAIN, 35));
+		g.drawString("Loading...", 770, 440);
+		g.drawRect(770, 450, 350, 50);
+		g.fillRect(770, 450, 250, 50);
 	}
 	
 	public void drawSplashPage(Graphics g) {
@@ -57,20 +74,47 @@ public class Panel extends JPanel implements KeyListener {
 		
 		
 	}
-	public void secondpage(Graphics g) {
+	public void drawSecondPage(Graphics g) {
 		g.setColor(Color.BLACK);
+		setBackground(new Color(255, 255, 255));
+		g.setFont(new Font("Arial", Font.PLAIN, 100));
+		g.setColor(new Color(0, 0, 0));
+		g.drawString("New Outfit", 700, 100);
+	
+		
+		g.setColor(new Color(183, 16, 37));
+		g.fillRoundRect(450, 700, 200, 100, 75, 75);
+		g.fillRoundRect(850, 700, 200, 100, 75, 75);
+		g.fillRoundRect(1250, 700, 200, 100, 75, 75);
+		g.setColor(new Color(255,255,255));
+		g.setFont(new Font("Arial", Font.PLAIN, 33));
+		g.drawString("Randomize", 470, 750);
+		g.drawString("Outfit", 510, 780);
+		g.drawString("Saved Outfits", 850, 760);
+		g.drawString("Menu", 1310, 760);
+		
+		g.drawImage(new ImageIcon("src/pictures/closet.png").getImage(), 800, 200, 400, 400, null);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		drawSplashPage(g);
-
+		drawLoadingScreen(g);
+		/*if (screenTracker.contentEquals("title_screen"))
+			drawSplashPage(g);
+		else
+			drawSecondPage(g);
+		*/
 	}
 
 	@Override
 	public void keyPressed(KeyEvent k) {
+		
+		if(k.getKeyCode()==KeyEvent.VK_ENTER) {
+			screenTracker="selection_screen";
+		}
+		
+		
 	}
 
 	@Override
